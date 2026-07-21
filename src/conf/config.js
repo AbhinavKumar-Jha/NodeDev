676f218d5ac285523e80dbf92c002e090866c1b6
Abhinav Kumar Jha <167169367+AbhinavKumar-Jha@users.noreply.github.com>
2026-07-21 14:09:46 +0530

    Update config.js

--- src/conf/config.js
+++ src/conf/config.js
@@ -1,9 +1,9 @@
 const conf = {
-    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL || "https://cloud.appwrite.io/v1"),
-    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID || ""),
-    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID || ""),
-    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID || ""),
-    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID || ""),
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL || "https://nyc.cloud.appwrite.io/v1"),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID || "6a5f15af00289a0a1221"),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID || "6a5f177f001dc733cf16"),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID || "posts"),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID || "blog-images"),
 };
 
 
