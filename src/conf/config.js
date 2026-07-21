6f40c4742ba2dc91c5d23d6eff2a8dd9797363ee
Abhinav Kumar Jha <167169367+AbhinavKumar-Jha@users.noreply.github.com>
2026-07-21 12:09:55 +0530

    Update config.js

--- src/conf/config.js
+++ src/conf/config.js
@@ -1,10 +1,11 @@
 const conf = {
-    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
-}
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL || "https://cloud.appwrite.io/v1"),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID || ""),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID || ""),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID || ""),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID || ""),
+};
+
 
 // we are doing it so that in production imports work as intended and it is available without using
 // lenghty import.meta.vite.----
