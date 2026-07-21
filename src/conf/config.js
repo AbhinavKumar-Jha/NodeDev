acc447569d22168adfa837bac0617d151949f771
Abhinav Kumar Jha <167169367+AbhinavKumar-Jha@users.noreply.github.com>
2026-07-21 15:06:30 +0530

    Update config.js

--- src/conf/config.js
+++ src/conf/config.js
@@ -10,7 +10,7 @@ const conf = {
     appwriteProjectId: getEnv(import.meta.env.VITE_APPWRITE_PROJECT_ID, "6a5f15af00289a0a1221"),
     appwriteDatabaseId: getEnv(import.meta.env.VITE_APPWRITE_DATABASE_ID, "6a5f177f001dc733cf16"),
     appwriteCollectionId: getEnv(import.meta.env.VITE_APPWRITE_COLLECTION_ID, "posts"),
-    appwriteBucketId: getEnv(import.meta.env.VITE_APPWRITE_BUCKET_ID, "blog-images"),
+    appwriteBucketId: getEnv(import.meta.env.VITE_APPWRITE_BUCKET_ID, "6a5f198a001f70879fd9"),
 };
 
 // export default conf;
